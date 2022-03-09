24edd01824352c11c2c6e9c93b7517606ed794ff
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-03-09 23:02:20 +0600

    s

--- lib/pages/messages_page.dart
+++ lib/pages/messages_page.dart
@@ -6,6 +6,7 @@ import 'package:chat_app2/constants/theme.dart';
 import 'package:chat_app2/widgets/common_widgets/avatar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:faker/faker.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:jiffy/jiffy.dart';
 
@@ -29,51 +30,7 @@ class MessagesPage extends StatelessWidget {
             final userDocs = snapShot.data!.docs;
             return CustomScrollView(
               slivers: [
-                SliverToBoxAdapter(
-                  child: Card(
-                    elevation: 0,
-                    child: SizedBox(
-                      height: 134,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Padding(
-                            padding:
-                                EdgeInsets.only(left: 16.0, top: 8, bottom: 16),
-                            child: Text(
-                              'Stories',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w900,
-                                fontSize: 15,
-                                color: AppColors.textFaded,
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: userDocs.length,
-                              itemBuilder: (BuildContext context, int index) {
-                                return Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: SizedBox(
-                                    width: 60,
-                                    child: _StoryCard(
-                                      storyData: StoryData(
-                                        name: userDocs[index]['userName'],
-                                        url: userDocs[index]['imageUrl'],
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+                _Stories(userDocs: userDocs),
                 SliverList(
                   delegate: SliverChildBuilderDelegate(
                     (context, index) {
@@ -102,49 +59,68 @@ class MessagesPage extends StatelessWidget {
 }
 
 class _Stories extends StatelessWidget {
-  const _Stories({Key? key, required this.usersList}) : super(key: key);
-  final Object usersList;
+  const _Stories({
+    Key? key,
+    required this.userDocs,
+  }) : super(key: key);
+
+  final List<QueryDocumentSnapshot<Object?>> userDocs;
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 0,
-      child: SizedBox(
-        height: 134,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(left: 16.0, top: 8, bottom: 16),
-              child: Text(
-                'Stories',
-                style: TextStyle(
-                  fontWeight: FontWeight.w900,
-                  fontSize: 15,
-                  color: AppColors.textFaded,
+    return SliverToBoxAdapter(
+      child: Card(
+        elevation: 0,
+        child: SizedBox(
+          height: 134,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 16.0, top: 8, bottom: 16),
+                child: Text(
+                  'Stories',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 15,
+                    color: AppColors.textFaded,
+                  ),
                 ),
               ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (BuildContext context, int index) {
-                  final faker = Faker();
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SizedBox(
-                      width: 60,
-                      child: _StoryCard(
-                        storyData: StoryData(
-                          name: faker.person.name(),
-                          url: Helpers.randomPictureUrl(),
+              Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: userDocs.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    if (index == 0) {
+                      return InkWell(
+                        child: Container(
+                          margin: const EdgeInsets.only(right: 12),
+                          child: const CircleAvatar(
+                            radius: 24,
+                            child: Icon(CupertinoIcons.add),
+                          ),
                         ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            )
-          ],
+                      );
+                    } else {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          width: 60,
+                          child: _StoryCard(
+                            storyData: StoryData(
+                              name: userDocs[index]['userName'],
+                              url: userDocs[index]['imageUrl'],
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
